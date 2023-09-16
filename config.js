8be671b0fb8dfbed0a51cfeb69a7c1424f573b41
Connexnkhambule <124759954+Connexnkhambule@users.noreply.github.com>
2023-09-17 00:46:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -41,23 +41,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
+global.owner = ['265995013516'] //ur owner number
+global.ownername = "NKHAMBULE ISHMAEL"
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
+global.botname = "GLITCH BOT-MD" //GLITCH BOT-MD
 global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "🦄Dream Guy Xeon"
+global.packname = "GLITCH BOT\n\n\n\n\n\n\n🦄🦄🦄 "
+global.author = "ISHMAEL NKHAMBULE"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
+global.ownertag = ['265995013516'] //ur owner tag
+global.ownernummenu = ['265995013516'] //ur owner number in menu and all
+global.watermark = "ISHMAEL NKHAMBULE." //ur watermark
 global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
 global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
 global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
